29cb7ebe9b9c1f5bf2e19583231168bcebdcacf0
Wenxin Fang <119010068@link.cuhk.edu.cn>
2025-07-28 14:17:19 -0700

    Include seperate functions to call in main

--- main.m
+++ main.m
@@ -2,25 +2,54 @@
 clear
 close all
 
-L1 = 40; % intralipid thickness in mm
-%L2 = 6.5; % dermis tissue thickness in mm
 
 tic
+
+%% ==== Variables ====
+SD = 30; % sender to detector distances, in mm
+%SDs = [20, 30, 40]
+hbt = 0.05; % total hemoglobin concentration, in mM
+% For two-layer model
+L1 = 40; % first layer thickness, in mm
+oxy_sim_all = [0.2 0.4 0.6 0.8 1]; % multiple oxygen levels
+
+
 %% ==== SD configuration ====
-SD = 30; % in mm 
 src_rad = 1; % in mm
 det_rad = 1; % in mm
 dim = [100, 100, 100]; % volume, in mm
-hbt = 0.05;
-excoef = [710 1058; 1075.44 691.32]; %[780 850]
-excoef = excoef * 2.303/1000;
-oxy_sim_all = [0.2 0.4 0.6 0.8 1];
+
 L_850_measured(2,5)=0;
 L_780_measured(2,5)=0;
-dim0 = dim(1);
-%src_coor = [dim0/2,(dim0 - SD(end))/2]; 
 src_coor = [50, 45];
 det_coor = [50, src_coor(2)+SD];
 unit = 1;
 
-layer=2;
+% Get coefficients mu_a, mu_a_s and mu_a_d
+[mu_a, mu_a_s, mu_a_d] = mu_a_calculation(hbt, oxy_sim_all);
+
+
+%% ==== MC configuration ====
+cfg.nphoton=5e5;
+cfg.vol=uint8(ones(100,100,100));
+cfg.vol(:,:,10:end)=2;    % add an inclusion
+cfg.prop=[0 0 1 1
+        mu_a(1,1) 11.1852 0.9 1.4
+        mu_a(1,1) 11.1852 0.9 1.4]; % [mua,mus,g,n]
+cfg.issrcfrom0=1;
+cfg.srcpos=[30 30 1];
+cfg.srcdir=[0 0 1];
+cfg.detpos=[30 20 1 1;30 40 1 1;20 30 1 1;40 30 1 1];
+cfg.vol(:,:,1)=0;   % pad a layer of 0s to get diffuse reflectance
+cfg.issaveref=1;
+cfg.gpuid=1;
+cfg.autopilot=1;
+cfg.tstart=0;
+cfg.tend=5e-9;
+cfg.tstep=5e-10;
+% calculate the fluence distribution with the given config
+[~,detpt]=mcxlabcl(cfg);
+
+% One layer example
+
+% Two layer example
--- one_layer.m
+++ one_layer.m
@@ -1,1 +1,32 @@
 % One layer model example
+function one_layer(hbt, sd, thick)
+    cfg.prop=[0 0 1 1
+        mu_a(1,1) 11.1852 0.9 1.4];
+    [~,detpt]=mcxlabcl(cfg);
+
+    for i = 1:5
+    %Iterating over mu_a
+        cfg.prop = [0 0 1 1
+            mu_a(1,i) 11.1852 0.9 1.4];
+        L = mcxmeanpath(detpt, cfg.prop);
+        dewt1=mcxdetweight(detpt, cfg.prop);
+        L_780_measured(:,i) = L';
+        %L_850_measured(i) = mcxmeanpath(detphoton, cfg.prop);
+    end
+
+    cfg.prop=[0 0 1 1
+        mu_a(2,1) 11.1852 0.9 1.4];
+
+    for i = 1:5
+    %Iterating over mu_a
+    cfg.prop = [0 0 1 1
+        mu_a(2,i) 11.1852 0.9 1.4];
+    
+    
+        % Calculate the measured L
+        %L_780_measured(i) = mcxmeanpath(detphoton, cfg.prop);
+        L = mcxmeanpath(detphoton, cfg.prop);
+        dewt2=mcxdetweight(detphoton, cfg.prop);
+        L_850_measured(:,i) = L';   
+    end
+end
